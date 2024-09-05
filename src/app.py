de856220be7f039d77a5ff4cc6ca7edccc006c6b
Dmitriy <lion95-10@mail.ru>
2024-09-05 17:52:09 +0300

    added config and connected database

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from config import SECRET_KEY, DATABASE_URI
 
 
 main_app = Flask(__name__)
-main_app.config['SECRET_KEY'] = 'fdgdfgdfggf786hfg6hfg6h7f'
-
+main_app.config['SECRET_KEY'] = SECRET_KEY
+main_app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+db = SQLAlchemy(main_app)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import logging
 
+import config
 from views import *
 from logger import configure_logging
 
 
 def run():
     """Запуск сервера и всех приложений"""
-    main_app.run(debug=True, host='0.0.0.0')
+    main_app.run(debug=config.DEBUG, host='0.0.0.0')
 
 
 if __name__ == '__main__':
